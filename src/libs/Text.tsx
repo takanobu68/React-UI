0f747627f2585154c57c2bf473bab0a003e2e50d
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-24 17:05:44 +0900

    コンポーネントのスタイル実装

--- src/libs/Text.tsx
+++ src/libs/Text.tsx
@@ -1,7 +1,13 @@
 import { VFC } from 'react';
+import styled from 'styled-components';
+import { fontSize } from './constants';
 
 type Props = { text: string };
 
 export const Text: VFC<Props> = ({ text }) => {
-  return <p>{text}</p>;
+  return <Wrapper>{text}</Wrapper>;
 };
+
+const Wrapper = styled.p`
+  font-size: ${fontSize.m};
+`;
