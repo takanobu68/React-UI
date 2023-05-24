550baca8283603ab6b7dc00988c4877c03e33c22
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-24 17:28:24 +0900

    Headingコンポーネントの使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { Text } from './libs/Text';
+import { Heading } from './libs/Heading';
 
 export const App = () => {
-  return <Text text='Hello World!' />;
+  return (
+    <>
+      <Text text='Hello World!' />
+      <Heading tag={'h1'}>見出し</Heading>
+    </>
+  );
 };
