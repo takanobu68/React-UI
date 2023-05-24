51506aaa801c478c7bcb757498c4b957f40d2fdf
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-24 18:10:44 +0900

    styleの動的な変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { Text } from './libs/Text';
 import { Heading } from './libs/Heading';
+import { Button } from './libs/Button';
 
 export const App = () => {
   return (
     <>
       <Text text='Hello World!' />
       <Heading tag={'h1'}>見出し</Heading>
+      <Button
+        onClick={() => console.log('clicked')}
+        title='Button'
+        type='primary'
+        width={96}
+      />
+      <Button
+        onClick={() => console.log('clicked')}
+        title='Button'
+        type='secondary'
+      />
+      <Button
+        onClick={() => console.log('clicked')}
+        title='Button'
+        type='error'
+      />
     </>
   );
 };
--- src/libs/Button.tsx
+++ src/libs/Button.tsx
@@ -1,4 +1,6 @@
 import { VFC } from 'react';
+import styled, { css } from 'styled-components';
+import { color, radius, space } from './constants';
 
 type ButtonType = 'primary' | 'secondary' | 'error';
 
@@ -9,6 +11,40 @@ type Props = {
   width?: number;
 };
 
-export const Button: VFC<Props> = ({ title, onClick }) => {
-  return <button onClick={onClick}>{title}</button>;
+export const Button: VFC<Props> = ({
+  title,
+  onClick,
+  type = 'primary',
+  width = 80,
+}) => {
+  return (
+    <Wrapper onClick={onClick} width={width} className={type}>
+      {title}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.button<{ width: number }>`
+  padding: ${space.m};
+  border-radius: ${radius.m};
+  border: solid 1px ${color.green};
+  background: ${color.green};
+  color: ${color.white};
+  text-align: center;
+  cursor: pointer;
+  box-sizing: border-box;
+  ${(props) =>
+    css`
+      width: ${props.width}px;
+    `}
+  &.secondary {
+    border: solid 1px ${color.gray};
+    background: ${color.white};
+    color: ${color.black};
+  }
+  &.error {
+    border: none;
+    background: ${color.red};
+    color: ${color.white};
+  }
+`;
